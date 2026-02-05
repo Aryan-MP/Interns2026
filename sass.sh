673f2288cc09e6b3394558cb4dcff2af18cf9cc0
fardeen <fardeenisa@gmail.com>
2026-02-05 14:20:01 +0530

    Task-SAAS Token based shell script completed Successfully

--- sass.sh
+++ sass.sh
@@ -5,7 +5,7 @@
 STORAGE_ACCOUNT="sdfghwer"
 CONTAINER="skkk12345"
 BLOB="file.txt"
-SAS_TOKEN="sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2026-02-05T16:14:16Z&st=2026-02-05T07:59:16Z&spr=https&sig=gAN0BvPEzjZNJnT%2BnX4PCOwdgDb5QNR37jfqUtHthX0%3D"
+SAS_TOKEN="sv=2024-11-04&ss=bfq=This is Sample Token "
 OUTPUT_FILE="./file.txt"
 
 # Construct full SAS URL
